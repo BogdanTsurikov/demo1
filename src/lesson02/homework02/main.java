703ad104ee095da27f144c2e5b9f48c01f8378e7
Богдан <Bogdanwarson@gmail.com>
2023-11-09 20:36:33 +0100

    lesson2

---
+++ src/lesson02/homework02/main.java
@@ -0,0 +1,26 @@
+package lesson02.homework02;
+
+public class main {
+    static int number = 1;
+    static float floatNumber = 4.8f;
+    static boolean bool = true;
+    static double doubleNumber = 1.2;
+    static char lastLetterOfTheAlphabet = 'Z';
+    static long longNumber = 1001;
+    static byte numberOfWheels = 4;
+    static short selfEsteem = -1;
+
+
+
+
+    public static void main(String[] args) {
+        System.out.print("int:" + number);
+        System.out.print("float:" + floatNumber);
+        System.out.print("boolean:" + bool);
+        System.out.print("double :" + doubleNumber);
+        System.out.print("char:" + lastLetterOfTheAlphabet);
+        System.out.print("long :" + longNumber);
+        System.out.print("byte:" + numberOfWheels);
+        System.out.print("short:" + selfEsteem);
+    }
+}
